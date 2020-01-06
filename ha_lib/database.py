3fffedd7df1d0c322ac53fe093fc4a315c7141f0
Vepnar <26712719+Vepnar@users.noreply.github.com>
2020-01-06 15:04:09 +0100

    Improved database

--- ha_lib/database.py
+++ ha_lib/database.py
@@ -1,187 +1,259 @@
-"""Module to access the database """
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Module to access the database
+Author: Arjan de Haan (Vepnar)
+Last edited: 6 January 2020"""
 
+import sys
 import time
 import sqlite3
 import os.path
 import asyncio
 from datetime import datetime
-from . import logger, processor
 
-DB = None
-
-
-def enable():
+class Database:
+    """Handle all the database functions.
+    args:
+        logger: (logger)
+        enabled: (boolean) enabled when this module is enabled
+        database_file: (str) path to database file
+        data_update_interval: (int) interval to check if we are in a new day
+        exit_on_crash: (boolean) exit module when crashes
     """
-    This is where the database will initialize.
-    After that we run some basic checks to confirm that everything is running like it should.
-    """
-    global DB
-    if not processor.config.getboolean('DATABASE', 'enabled'):
-        return
 
-    file = processor.config.get('DATABASE', 'file')
-    try:
-        if os.path.isfile(file):
-            logger.debug('Database loaded')
-            DB = sqlite3.connect(file)
+    def __init__(self, logger, enabled, database_file,
+                 exit_on_crash=True, data_update_interval=36000):
+        self.logger = logger
+        self.enabled = enabled
+        self.data_update_interval = data_update_interval
+
+        if not enabled:
             return
-        logger.debug('Creating new database file')
-        DB = sqlite3.connect(file)
-        DB.executescript('''CREATE TABLE RECORDS(TIMESTAMP INTEGER NOT NULL PRIMARY KEY, RECEIVED\
-            INTEGER NOT NULL, SEND INTEGER NOT NULL, SPECIAL INTEGER NOT NULL); CREATE TABLE \
-            DAYLOGS (TIMESTAMP INTEGER NOT NULL PRIMARY KEY, RECEIVED INTEGER NOT NULL, SEND \
-            INTEGER NOT NULL); CREATE TABLE MONTHLOGS (TIMESTAMP INTEGER NOT NULL PRIMARY \
-            KEY, RECEIVED INTEGER NOT NULL, SEND INTEGER NOT NULL);
-        ''')
-        DB.commit()
-        logger.debug('Database loaded')
 
-    except sqlite3.Error:
-        processor.config.set('DATABASE', 'enabled', 'False')
-        logger.err(
-            'Can\'t open the database. Check if this user has permissions to write')
+        sql = '''
+        CREATE TABLE RECORDS (
+        TIMESTAMP INTEGER NOT NULL PRIMARY KEY,
+        RECEIVED INTEGER NOT NULL, SEND INTEGER NOT NULL, SPECIAL INTEGER NOT NULL);
+        CREATE TABLE DAYLOGS (
+        TIMESTAMP INTEGER NOT NULL PRIMARY KEY, RECEIVED INTEGER
+        NOT NULL, SEND INTEGER NOT NULL); 
+        CREATE TABLE MONTHLOGS (
+        TIMESTAMP INTEGER NOT
+        NULL PRIMARY KEY, RECEIVED INTEGER NOT NULL, SEND INTEGER NOT NULL);
+        '''
 
+        try:
+            if os.path.isfile(database_file):
+                self.logger.debug('Database loaded')
+                self.database = sqlite3.connect(database_file)
+                return
+            self.logger.debug('Creating new database file')
+            self.database = sqlite3.connect(database_file)
+            self.database.executescript(sql)
+            self.database.commit()
+            self.logger.debug('Database loaded')
 
-def get_last_value(received_bytes=0, send_bytes=0):
-    """Receive total amount of network usage from the database.
+        except sqlite3.Error:
+            if exit_on_crash:
+                sys.exit(-1)
+            else:
+                raise Exception
+            self.logger.err(
+                'Can\'t open the database. Check if this user has permissions to write')
 
-    Will access RECORDS table in the database to receive data about the total usage.
-    It will do the same for DAYLOGS when there is no data in the RECORDS table.
+    def get_last_value(self, received_bytes=0, send_bytes=0, throw=False):
+        """Receive total amount of network usage from the database.
 
-    Args:
-        received_bytes: total amount of bytes received today.
-        send_bytes: total amount of bytes send today.
-    Return:
-        Total received bytes according to the database.
-        Total send bytes according to the database
-    """
+        Will access RECORDS table in the database to receive data about the total usage.
+        It will do the same for DAYLOGS when there is no data in the RECORDS table.
 
-    if not processor.config.getboolean('DATABASE', 'enabled'):
-        return received_bytes, send_bytes
-    sql = 'SELECT RECEIVED, SEND FROM RECORDS WHERE TIMESTAMP =\
-         (SELECT MAX(TIMESTAMP) FROM RECORDS);'
-    output = DB.execute(sql).fetchone()
+        Args:
+            received_bytes: total amount of bytes received today.
+            send_bytes: total amount of bytes send today.
+            throw: throw an error when an error occurs
+        Return:
+            Total received bytes according to the database.
+            Total send bytes according to the database
+        """
 
-    if output is None:  # Access daylogs when there is nothing found in records
-        sql = 'SELECT RECEIVED, SEND FROM DAYLOGS WHERE TIMESTAMP =\
-             (SELECT MAX(TIMESTAMP) FROM DAYLOGS);'
-        output = DB.execute(sql).fetchone()
+        if not self.enabled:
+            if throw:
+                raise Exception
+            return received_bytes, send_bytes
+
+        sql = 'SELECT RECEIVED, SEND FROM RECORDS WHERE TIMESTAMP =\
+            (SELECT MAX(TIMESTAMP) FROM RECORDS);'
+        output = self.database.execute(sql).fetchone()
+
+        if output is None:  # Access daylogs when there is nothing found in records
+            sql = 'SELECT RECEIVED, SEND FROM DAYLOGS WHERE TIMESTAMP =\
+                (SELECT MAX(TIMESTAMP) FROM DAYLOGS);'
+            output = self.database.execute(sql).fetchone()
+            if output is None:
+                return 0, 0
+            return output[0], output[1]
+        return output[0], output[1]
+
+    # Is this used??
+    def get_start_value(self):
+        """Receive today's starting value
+        Returns:
+            Received bytes
+            Send bytes
+        """
+        if not self.enabled:
+            return 0, 0, 0
+        sql = '''
+        SELECT
+        RECEIVED,SEND 
+        FROM RECORDS 
+        WHERE
+        TIMESTAMP =(SELECT MIN(TIMESTAMP) FROM RECORDS);
+        '''
+
+        output = self.database.execute(sql).fetchone()
         if output is None:
             return 0, 0
         return output[0], output[1]
-    return output[0], output[1]
 
 
-def get_start_value():
-    """Receive today's starting value
-    Returns:
-        Received bytes
-        Send bytes
-    """
-    if not processor.config.getboolean('DATABASE', 'enabled'):
-        return 0, 0, 0
-    sql = 'SELECT RECEIVED,SEND FROM RECORDS WHERE TIMESTAMP =\
-         (SELECT MIN(TIMESTAMP) FROM RECORDS);'
-    output = DB.execute(sql).fetchone()
-    if output is None:
-        return 0, 0
-    return output[0], output[1]
+    def get_timestamps(self):
+        """Receive daily timestamp and record's timestamp
+        Returns:
+            record: timestamp of the first records in today logs.
+            daily: timestamp of the first record in the daily logs.
+        """
+        record, daily = 0, 0
 
+        # Receive the first timestamp of the records table.
+        sql = 'SELECT TIMESTAMP FROM RECORDS WHERE TIMESTAMP=\
+            (SELECT MIN(TIMESTAMP)  FROM RECORDS);'
+        output = self.database.execute(sql).fetchone()
+        if output is not None:
+            record = output[0]
 
-def get_timestamps():
-    """Receive daily timestamp and record's timestamp
-    Returns:
-        today: timestamp of the first records in today logs
-        daily: timestamp of the first record in the daily logs
-    """
-    today, daily = 0, 0
-    if not processor.config.getboolean('DATABASE', 'enabled'):
-        return today, daily
+        # Receive the first row in the daily logs
+        sql = 'SELECT TIMESTAMP FROM RECORDS WHERE TIMESTAMP =\
+            (SELECT MIN(TIMESTAMP)  FROM DAYLOGS);'
+        output = self.database.execute(sql).fetchone()
+        if output is not None:
+            daily = output[0]
+        return record, daily
 
-    # Receive the first timestamp of today.
-    sql = 'SELECT TIMESTAMP FROM RECORDS WHERE TIMESTAMP =\
-         (SELECT MIN(TIMESTAMP)  FROM RECORDS);'
-    output = DB.execute(sql).fetchone()
-    if output is not None:
-        today = output[0]
-    # Receive the first row in the daily logs
-    sql = 'SELECT TIMESTAMP FROM RECORDS WHERE TIMESTAMP =\
-         (SELECT MIN(TIMESTAMP)  FROM DAYLOGS);'
-    output = DB.execute(sql).fetchone()
-    if output is not None:
-        daily = output[0]
-    return today, daily
+    def add_row(self, received, send, timestamp=None, special=0, throw=False):
+        """Adds new information to the database
 
+        Args:
+            received: amount of bytes received.
+            send: amount of bytes send.
+            timestamp: timestamp when this is information is captured.
+            special:
+                0 = nothing special.
+                1 = captured on startup.
+            throw: throw exception when an error occurs
+        """
+        if not self.enabled:
+            return False
 
-def add_row(received, send, timestamp=None, special=0):
-    """Adds new information to the database
+        if timestamp is None:
+            timestamp = int(time.time())
 
-    Args:
-        received: amount of bytes received.
-        send: amount of bytes send.
-        timestamp: timestamp when this is information is captured.
-        special:
-            0 = nothing special.
-            1 = captured on startup.
-    """
-    if not processor.config.getboolean('DATABASE', 'enabled'):
-        return
+        sql = 'INSERT INTO RECORDS (TIMESTAMP, RECEIVED, SEND, SPECIAL) ' \
+            f'VALUES({timestamp}, {received}, {send},{special});'
+        try:
+            self.database.execute(sql)
+            self.database.commit()
+            return True
+        except sqlite3.Error:
+            if not throw:
+                self.logger.warn('Couldn\'t write to the database')
+            else:
+                raise Exception
+        return False
 
-    if timestamp is None:
+    async def loop(self):
+        """Asynchronous infinite loop to update the database.
+        This means that i'll delete old information.
+        """
+
+        if not self.enabled:
+            return
+        if self.data_update_interval < 6:
+            return
+        while True:
+            await asyncio.sleep(self.data_update_interval)
+            if not self.check_new_month():
+                self.check_new_day()
+
+    def check_new_day(self, throw=False):
+        """Function to check if we are in a new day
+        Args:
+            throw: (boolean) throw exception when something goes wrong.
+        """
+        record_timestamp, _ = self.get_timestamps()
+        record_date = datetime.fromtimestamp(record_timestamp)
+        received, send = self.get_last_value(0, 0)
         timestamp = int(time.time())
-
-    sql = 'INSERT INTO RECORDS (TIMESTAMP, RECEIVED, SEND, SPECIAL) ' \
-        f'VALUES({timestamp}, {received}, {send},{special});'
-    try:
-        DB.execute(sql)
-        DB.commit()
-    except sqlite3.Error:
-        logger.warn('Couldn\'t write to the database')
-
-async def loop():
-    """Asynchronous infinite loop to update the database.
-    This means that i'll delete old information."""
-
-    if not processor.config.getboolean('DATABASE', 'enabled'):
-        return
-    interval = processor.config.getint('DATABASE', 'datamoveinterval')
-    if interval < 6:
-        return      
-    while True:
-        await asyncio.sleep(interval)
-        today_timestamp, daily_timestamp = get_timestamps()
-        if today_timestamp == 0:
-            continue
-        today_date = datetime.fromtimestamp(today_timestamp)
-        daily_date = datetime.fromtimestamp(daily_timestamp)
-        received, send = get_last_value(0, 0)
         new_date = datetime.now()
-        timestamp = int(datetime.timestamp(new_date))
-        if new_date.month != daily_date.month and daily_timestamp != 0:
-            sql = 'INSERT INTO MONTHLOGS (TIMESTAMP, RECEIVED, SEND) ' \
-                f'VALUES({timestamp}, {received}, {send});'
-            DB.execute(sql)
-            DB.execute('DELETE FROM RECORDS')
-            DB.execute('DELETE FROM DAYLOGS')
-            try:
-                DB.commit()
-                logger.debug(
-                    'New month! old information has been purged and stored in a more compact way')
-            except sqlite3.Error:
-                logger.warn('Couldn\'t write to the database')
-            continue
+
+        if throw: # Set 32 days back for the testing module
+            timestamp -= 165888000
+
+        if record_timestamp == 0:
+            if throw:
+                raise sqlite3.Error
+            return False
 
         # Check if we are in a new day.
-        if abs(new_date - today_date).days < 1:
-            continue
+        if abs(new_date - record_date).days < 1:
+            if throw:
+                raise sqlite3.Error
+            return False
 
         sql = 'INSERT INTO DAYLOGS (TIMESTAMP, RECEIVED, SEND) ' \
             f'VALUES({timestamp}, {received}, {send});'
-        DB.execute(sql)
-        DB.execute('DELETE FROM RECORDS')
+        self.database.execute(sql)
+        self.database.execute('DELETE FROM RECORDS')
         try:
-            DB.commit()
-            logger.debug(
+            self.database.commit()
+            self.logger.debug(
                 'New day! old information has been purged and stored in a more compact way')
+            return True
         except sqlite3.Error:
-            logger.warn('Couldn\'t write to the database')
+            self.logger.warn('Couldn\'t write to the database')
+            if throw:
+                raise sqlite3.Error
+        return False
+
+    def check_new_month(self, throw=False):
+        """Function to check if we are in a new month
+        Args:
+            throw: (boolean) throw exception when something goes wrong.
+        """
+        _, daily_timestamp = self.get_timestamps()
+        daily_date = datetime.fromtimestamp(daily_timestamp)
+
+        received, send = self.get_last_value(0, 0)
+        new_date = datetime.now()
+        timestamp = int(time.time())
+
+        if not new_date.month != daily_date.month and daily_timestamp != 0:
+            if throw:
+                raise Exception
+            return False
+
+        sql = 'INSERT INTO MONTHLOGS (TIMESTAMP, RECEIVED, SEND) ' \
+            f'VALUES({timestamp}, {received}, {send});'
+        self.database.execute(sql)
+        self.database.execute('DELETE FROM RECORDS')
+        self.database.execute('DELETE FROM DAYLOGS')
+        try:
+            self.database.commit()
+            self.logger.debug(
+                'New month! old information has been purged and stored in a more compact way')
+            return True
+        except sqlite3.Error:
+            self.logger.warn('Couldn\'t write to the database')
+            if throw:
+                raise sqlite3.Error
+            return False
